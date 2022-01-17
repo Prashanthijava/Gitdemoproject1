c7fe0c6822162e6b13957c3445864438aff92ff1
prash <prash@DESKTOP-B76222K.lan>
2022-01-17 09:27:56 -0600

    This is my initial commit

---
+++ src/JavagitPackage/TestJava.java
@@ -0,0 +1,16 @@
+package JavagitPackage;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+
+		String hungry = "yes";
+
+		if (hungry == "yes" || hungry == "Y") {
+			System.out.println("Eat something");
+		} else {
+			System.out.println("do work");
+		}
+	}
+
+}
